655321f17015e5e7c65d1aeeeb7456ea5d1535f8
Brian Salehi <briansalehi@proton.me>
2025-11-30 20:42:53 +0100

    property link targets to tests

--- cmake/test_utils.cmake
+++ cmake/test_utils.cmake
@@ -1,47 +1,30 @@
 cmake_minimum_required(VERSION 3.31)
 
 function(define_tests)
-    set(options EXECUTABLE LIBRARY)
-    set(one_value NAME TARGET)
-    set(multi_value SOURCES INCLUDE_DIRS)
-    cmake_parse_arguments(TEST_MODULE "${options}" "${one_value}" "${multi_value}" ${ARGN})
+    set(options)
+    set(single_args TARGET)
+    set(multi_args FILES MOCK_DIRS)
+    cmake_parse_arguments(DEFINE_TESTS "${options}" "${single_args}" "${multi_args}" ${ARGN})
 
-    message(DEBUG "Unit test ${TEST_MODULE_NAME}")
-
-    if(NOT TARGET ${TEST_MODULE_TARGET})
-        message(FATAL_ERROR "${TEST_MODULE_TARGET} is not a target, but define_tests macro requires an executable or a library target")
+    if(NOT TARGET ${DEFINE_TESTS_TARGET})
+        message(FATAL_ERROR "${DEFINE_TESTS_TARGET} is not a target, but define_tests() requires a library target")
     endif()
 
-    get_target_property(dependencies ${TEST_MODULE_TARGET} LINK_LIBRARIES)
-
     # defines an individual target for each unit test
-    foreach(test_file ${TEST_MODULE_SOURCES})
+    foreach(test_file ${DEFINE_TESTS_FILES})
         get_filename_component(test_unit ${test_file} NAME_WE)
-        add_executable(${test_unit} ${test_file})
-        target_include_directories(${test_unit} PRIVATE ${TEST_MODULE_INCLUDE_DIRS})
-
-        if(TEST_MODULE_LIBRARY)
-            target_link_libraries(${test_unit} PRIVATE ${TEST_MODULE_TARGET} ${dependencies} gtest gtest_main)
-        elseif(TEST_MODULE_EXECUTABLE)
-            target_link_libraries(${test_unit} PRIVATE ${dependencies} gtest gtest_main)
-        else()
-            message(FATAL_ERROR "Test module type is not correct or not supported")
-        endif()
-
+        add_executable(${test_unit})
+        target_sources(${test_unit} PRIVATE ${test_file})
+        target_include_directories(${test_unit} PRIVATE ${DEFINE_TESTS_MOCK_DIRS})
+        target_link_libraries(${test_unit} PRIVATE ${DEFINE_TESTS_TARGET} gtest gtest_main)
         add_test(NAME ${test_unit} COMMAND ${test_unit})
         list(APPEND unit_tests ${test_unit})
     endforeach()
 
     # defines a target running all unit tests in this module
-    add_executable(${TEST_MODULE_TARGET}-tests)
-    target_sources(${TEST_MODULE_TARGET}-tests PRIVATE ${TEST_MODULE_SOURCES})
-    target_include_directories(${TEST_MODULE_TARGET}-tests PRIVATE ${TEST_MODULE_INCLUDE_DIRS})
-
-    if(TEST_MODULE_LIBRARY)
-        target_link_libraries(${TEST_MODULE_TARGET}-tests PRIVATE ${TEST_MODULE_TARGET} ${dependencies} gtest gtest_main)
-    elseif(TEST_MODULE_EXECUTABLE)
-        target_link_libraries(${TEST_MODULE_TARGET}-tests PRIVATE ${dependencies} gtest gtest_main)
-    endif()
-
-    add_test(NAME ${TEST_MODULE_TARGET}-tests COMMAND ${TEST_MODULE_TARGET}-tests)
+    add_executable(${DEFINE_TESTS_TARGET}-tests)
+    target_sources(${DEFINE_TESTS_TARGET}-tests PRIVATE ${DEFINE_TESTS_FILES})
+    target_include_directories(${DEFINE_TESTS_TARGET}-tests PRIVATE ${DEFINE_TESTS_MOCK_DIRS})
+    target_link_libraries(${DEFINE_TESTS_TARGET}-tests PRIVATE ${DEFINE_TESTS_TARGET} gtest gtest_main)
+    add_test(NAME ${DEFINE_TESTS_TARGET}-tests COMMAND ${DEFINE_TESTS_TARGET}-tests)
 endfunction()
