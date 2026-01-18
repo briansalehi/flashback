0aedc37c8c4cfcf2522fd8f7651cf39436bb8b9d
Brian Salehi <briansalehi@proton.me>
2026-01-18 22:12:40 +0100

    database: script to create test database

--- database/create-test-database.sh
+++ database/create-test-database.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-psql -U postgres -c 'create database flashback_test with owner flashback connection limit 3'
-psql -U flashback -d flashback_test -f schema.sql
+sudo -u postgres psql -c 'create role flashback with login connection limit 3'
+sudo -u postgres psql -c 'drop database if exists flashback_test'
+sudo -u postgres psql -c 'create database flashback_test with owner flashback connection limit 3'
+sudo -u postgres psql -c 'create extension if not exists pg_trgm with schema flashback'
+sudo -u postgres psql -d flashback_test -f database/schema.sql
