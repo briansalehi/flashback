1885e3ea24ade1da68a84631627180a025a8fdf1
Brian Salehi <briansalehi@proton.me>
2026-02-03 20:47:54 +0100

    server: add known methods from database
    Signed-off-by: Brian Salehi <briansalehi@proton.me>

--- daemon/include/flashback/server.hpp
+++ daemon/include/flashback/server.hpp
@@ -13,46 +13,83 @@ public:
     explicit server(std::shared_ptr<basic_database> database);
     ~server() override = default;
 
-    // entry page requests
+    // entry page
     grpc::Status SignUp(grpc::ServerContext* context, SignUpRequest const* request, SignUpResponse* response) override;
     grpc::Status SignIn(grpc::ServerContext* context, SignInRequest const* request, SignInResponse* response) override;
     grpc::Status VerifySession(grpc::ServerContext* context, VerifySessionRequest const* request, VerifySessionResponse* response) override;
     // VerifyUser
 
-    // accounts
+    // accounts page
     grpc::Status ResetPassword(grpc::ServerContext* context, ResetPasswordRequest const* request, ResetPasswordResponse* response) override;
     // RenameUser
     // ChangeUserEmail
 
-    // welcome page requests
-    grpc::Status CreateRoadmap(grpc::ServerContext* context, CreateRoadmapRequest const* request, CreateRoadmapResponse* response) override;
+    // home page
     grpc::Status GetRoadmaps(grpc::ServerContext* context, GetRoadmapsRequest const* request, GetRoadmapsResponse* response) override;
+    // GetStudyResources
+
+    // roadmap page
+    grpc::Status CreateRoadmap(grpc::ServerContext* context, CreateRoadmapRequest const* request, CreateRoadmapResponse* response) override;
     grpc::Status RenameRoadmap(grpc::ServerContext* context, RenameRoadmapRequest const* request, RenameRoadmapResponse* response) override;
     grpc::Status RemoveRoadmap(grpc::ServerContext* context, RemoveRoadmapRequest const* request, RemoveRoadmapResponse* response) override;
     grpc::Status SearchRoadmaps(grpc::ServerContext* context, SearchRoadmapsRequest const* request, SearchRoadmapsResponse* response) override;
     grpc::Status CloneRoadmap(grpc::ServerContext* context, CloneRoadmapRequest const* request, CloneRoadmapResponse* response) override;
-    // GetRoadmapWeight
 
-    // roadmap page requests
+    // milestone page
     grpc::Status GetMilestones(grpc::ServerContext* context, GetMilestonesRequest const* request, GetMilestonesResponse* response) override;
     grpc::Status AddMilestone(grpc::ServerContext* context, AddMilestoneRequest const* request, AddMilestoneResponse* response) override;
     grpc::Status AddRequirement(grpc::ServerContext* context, AddRequirementRequest const* request, AddRequirementResponse* response) override;
     grpc::Status GetRequirements(grpc::ServerContext* context, GetRequirementsRequest const* request, GetRequirementsResponse* response) override;
-    grpc::Status CreateSubject(grpc::ServerContext* context, CreateSubjectRequest const* request, CreateSubjectResponse* response) override;
-    grpc::Status SearchSubjects(grpc::ServerContext* context, SearchSubjectsRequest const* request, SearchSubjectsResponse* response) override;
     grpc::Status ReorderMilestone(grpc::ServerContext* context, ReorderMilestoneRequest const* request, ReorderMilestoneResponse* response) override;
     grpc::Status RemoveMilestone(grpc::ServerContext* context, RemoveMilestoneRequest const* request, RemoveMilestoneResponse* response) override;
     grpc::Status ChangeMilestoneLevel(grpc::ServerContext* context, ChangeMilestoneLevelRequest const* request, ChangeMilestoneLevelResponse* response) override;
 
-    // subject page requests
+    // subject page
+    grpc::Status CreateSubject(grpc::ServerContext* context, CreateSubjectRequest const* request, CreateSubjectResponse* response) override;
+    grpc::Status SearchSubjects(grpc::ServerContext* context, SearchSubjectsRequest const* request, SearchSubjectsResponse* response) override;
     grpc::Status RenameSubject(grpc::ServerContext* context, RenameSubjectRequest const* request, RenameSubjectResponse* response) override;
     grpc::Status RemoveSubject(grpc::ServerContext* context, RemoveSubjectRequest const* request, RemoveSubjectResponse* response) override;
     grpc::Status MergeSubjects(grpc::ServerContext* context, MergeSubjectsRequest const* request, MergeSubjectsResponse* response) override;
+
+    // resource page
     // GetResources
-    // AddResourceToSubject
     // CreateResource
+    // AddResourceToSubject
     // DropResourceFromSubject
+    // SearchResources
     // MergeResources
+    // RenameResource
+    // RemoveResource
+    // EditResourceProduction
+    // EditResourceExpiration
+    // EditResourceLink
+    // ChangeResourceType
+    // ChangeSectionPattern
+    // GetRelevantSubjects
+
+    // nerves
+    // CreateNerve
+    // GetNerves
+
+    // providers
+    // CreateProvider
+    // AddProvider
+    // DropProvider
+    // SearchProviders
+    // RenameProvider
+    // RemoveProvider
+    // MergeProviders
+
+    // presenters
+    // CreatePresenter
+    // AddPresenter
+    // DropPresenter
+    // SearchPresenters
+    // RenamePresenters
+    // RemovePresenters
+    // MergePresenters
+
+    // topic page
     // GetTopics
     // CreateTopic
     // ReorderTopic
@@ -60,54 +97,62 @@ public:
     // MergeTopics
     // RenameTopic
     // MoveTopic
+    // SearchTopics
+    // ChangeTopicLevel
 
-    // resource page requests
+    // section page
     // GetSections
-    // RenameResource
-    // RemoveResource
-    // EditResourceProvider
-    // EditResourcePresenter
-    // EditResourceLink
-    // ChangeResourceType
-    // ChangeSectionPattern
-    // ChangeCondition
     // CreateSection
     // ReorderSection
     // RemoveSection
     // MergeSections
     // RenameSection
     // MoveSection
+    // SearchSections
+    // EditSectionLink
 
-    // section/topic page
+    // shared functions in section and topic pages
     // CreateCard
+    // AddCardToSection
+    // AddCardToTopic
     // ReorderCard
     // RemoveCard
     // MergeCards
+    // SearchCards
 
     // section page
     // GetStudyCards
-    // RenameSection
-    // RemoveSection
     // MoveCardToSection
+    // MarkSectionAsReviewed
 
     // topic page
     // GetPracticeCards
-    // RenameTopic
-    // RemoveTopic
     // MoveCardToTopic
+    // CreateAssessment
+    // ExpandAssessment
+    // DiminishAssessment
+    // GetAssimilationCoverage
+    // IsAssimilated
 
     // card page
     // EditHeadline
     // CreateBlock
     // GetBlocks
-    // EditBlock
+    // EditBlockContent
     // RemoveBlock
     // EditBlockExtension
-    // EditBlockType
+    // ChangeBlockType
     // EditBlockMetaData
     // ReorderBlock
     // MergeBlocks
     // SplitBlock
+    // MarkCardAsReviewed
+    // GetVariations
+
+    // progress
+    // Study
+    // MakeProgress
+    // GetProgressWeight
 
 protected:
     [[nodiscard]] static std::string calculate_hash(std::string_view password);
