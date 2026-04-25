414996700b23a2788f539c0c06e2e574ef8287d4
Brian Salehi <briansalehi@proton.me>
2026-04-25 10:46:02 +0200

    workflow: clone database before synchronization

--- database/sync-test-database.sh
+++ database/sync-test-database.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 base_dir=""
+temp_database="$(mktemp -d)/flashback.sql"
 
 if [ "$USER" != "postgres" ]
 then
@@ -24,15 +25,46 @@ else
     exit 2
 fi
 
-if ! pg_isready
+database="$base_dir/flashback.sql"
+
+if ! pg_isready 1>/dev/null
 then
-    pg_ctl start -D /var/lib/pgsql/data -l /var/lib/pgsql/logfile
+    echo -e "\e[1;31mdatabase is not running\e[0m "
+    exit 1
 fi
 
-psql -c 'drop database flashback_test'
-psql -c 'create database flashback_test with owner flashback connection limit 10'
-psql -c 'alter role brian in database flashback_test set role = flashback'
-psql -c 'alter role flashback_client in database flashback_test set role = flashback'
-psql -c 'alter role brian in database flashback_test set search_path = flashback, public'
-psql -c 'alter role flashback_client in database flashback_test set search_path = flashback, public'
-psql -d flashback_test -f "$base_dir/flashback.sql" --schema-only -q
+echo -ne "\e[1;35mdropping previous test database:\e[0m "
+psql -c 'drop database flashback_test' || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mcreating a new test database:\e[0m "
+psql -c 'create database flashback_test with owner flashback connection limit 10' || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mloading live data to the test database\e[0m "
+psql -d flashback_test -f "$database" -q 1>/dev/null && echo || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mdumping database schema to a temporary file\e[0m "
+pg_dump -d flashback_test -f "$temp_database" --schema-only && echo || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mdropping test database with live data:\e[0m "
+psql -c 'drop database flashback_test' || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mcreating a new test database:\e[0m "
+psql -c 'create database flashback_test with owner flashback connection limit 10' || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mloading database schema into the test database\e[0m "
+psql -d flashback_test -f "$temp_database" -q 1>/dev/null && echo || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mchanging the role of user brian:\e[0m "
+psql -c 'alter role brian in database flashback_test set role = flashback'|| echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mchanging the role of user flashback client:\e[0m "
+psql -c 'alter role flashback_client in database flashback_test set role = flashback'|| echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mchanging the search path fo user brian:\e[0m "
+psql -c 'alter role brian in database flashback_test set search_path = flashback, public'|| echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mchanging the search path fo user flashback client:\e[0m "
+psql -c 'alter role flashback_client in database flashback_test set search_path = flashback, public' || echo -e "\e[1;31mfailed\e[0m"
+
+echo -ne "\e[1;35mremoving temporary database file\e[0m "
+rm -r "$(dirname "$temp_database")" && echo || echo -e "\e[1;31mfailed\e[0m"
